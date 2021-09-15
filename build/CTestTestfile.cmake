7ec0376ec9f3ce2e501314fa70814542226afa84
vishal <vishal.b@sigmasolutions.co.in>
2021-09-15 19:09:28 +0530

    updated the plugins and worked on navigation stack

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/vishal/catkin_ws/src/apostile_robot
+# Build directory: /home/vishal/catkin_ws/src/apostile_robot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
